cdb1ea425624a10ec6808a05294068cf6b54a3a5
Dave Carrigan <dave@openshut.net>
2024-10-10 13:13:30 -0700

    Track FMS version of RxSwift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/ReactiveX/RxSwift",
+            url: "https://github.com/FountainheadMobileSolutions/RxSwift",
             .upToNextMajor(from: "6.0.0")
         )
     ],
